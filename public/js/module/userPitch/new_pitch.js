9f79dd78208fee22d370aa30ee256d80afd91e57
Maulik <mk@narola.email>
2018-07-26 18:07:03 +0530

    HEADER UPDATE || PREVIEW VIDEO|IMAGE|PDF

--- public/js/module/userPitch/new_pitch.js
+++ public/js/module/userPitch/new_pitch.js
@@ -1,57 +1,92 @@
 const newPitch = function () {
+
+    const handlePreview = (event) => {
+
+    }
     const handleNewPitchFormUI = () => {
         $('.placeholder').hide();
+        $('.preview_file').hide();
+        $('.preview_file_image').hide();
+        $('.preview_file_docs').hide();
         $('#main-box').addClass('active_one');
         $('#continue_btn_main').on("click", function () {
             $('.active_one').hide();
             $('#main-box').removeClass('active_one');
-            $('#placeholder-box').show('200');
-            $('#placeholder-box').addClass('active_one');
+            $('.active_one').hide();
+            $('div').removeClass('active_one');
+            $(".current_preview").show('200');
         });
         $(document).on("click", '.a_another_btn', function () {
+            $('.current_preview').hide();
             $('.active_one').hide();
             $('.add_another_o').hide();
             $(".add_another_o").removeClass("active_one");
             $(".another-page-blank").clone().appendTo(".add_box_here").addClass('add_another_o active_one').removeClass('another-page-blank');
             $('.add_another_o').show('200');
         })
-        $(document).on("click",'.continue_btn',function(){
+        $(document).on("click", '.continue_btn', function () {
             $('.active_one').hide();
             $('div').removeClass('active_one');
-            $('#placeholder-box').show('200');
-            $('#placeholder-box').addClass('active_one');
+            $(".current_preview").show('200');
+            //$('#placeholder-box').addClass('active_one');
         })
-        $(document).on("click",'.continue_btn',function(){
-            $('.active_one').hide();
-            $('div').removeClass('active_one');
-            $('#placeholder-box-2').show('200');
-            $('#placeholder-box').addClass('active_one');
-        })
+        // $(document).on("click",'.continue_btn',function(){
+        //     $('.active_one').hide();
+        //     $('div').removeClass('active_one');
+        //     $('#placeholder-box-2').show('200');
+        //     $('#placeholder-box').addClass('active_one');
+        // })
+
     }
 
     const handleDropZone = () => {
         $(document).on("change", '.drop_zone_input', function () {
-            console.log('hasdas');
             var $element = $(this);
             var $input = $element;
             var $value = $(this).parent('.file').find('.file-value');
-            console.log($value);
-             // Get the value of the input
-             var val = $input.val();
-             // Normalize strings    
-             val = val.replace(/\\/g, "/");
-             // Remove the path
-             val = val.substring(val.lastIndexOf("/") + 1);
-             // Toggle the 'active' class based
-             // on whether or not there is a value
-             $element.toggleClass('active', !!val.length);
-             // Set the value text accordingly
-             $value.text(val);
-             console.log(val);
-             $(this).parent('.file').html(val);
-             $(this).addClass('focus');
-        })
+            // Get the value of the input
+            var val = $input.val();
+            // Normalize strings    
+            val = val.replace(/\\/g, "/");
+            // Remove the path
+            val = val.substring(val.lastIndexOf("/") + 1);
+            // Toggle the 'active' class based
+            // on whether or not there is a value
+            $element.toggleClass('active', !!val.length);
+            // Set the value text accordingly
+            $value.text(val);
+            $(this).parent('.file').html(val);
+            $(this).addClass('focus');
+            // 
+            var fileExtensionVideo = ['avi', 'wmv', 'mov', '3gp', 'mp4'];
+            var fileExtensionImage = ['png', 'jpeg', 'jpg','bmp'];
+            var fileExtensionDocs = ['pdf'];
+            var filename = val;
+            if (jQuery.inArray(jQuery.trim(filename.split('.').pop().toLowerCase()), fileExtensionVideo) != -1) {
+                // Video Preview
+                $('div').removeClass('current_preview');
+                $(".preview_file").clone().appendTo(".add_preview").addClass('current_preview active_one').removeClass('preview_file');
+                let fileUrl = $(this)[0].src = URL.createObjectURL(this.files[0]);
+                $(".current_preview .video_here").attr("src", fileUrl);
+            }
+            else if(jQuery.inArray(jQuery.trim(filename.split('.').pop().toLowerCase()), fileExtensionImage) != -1) {
+                // Image Preview
+                $('div').removeClass('current_preview');
+               $(".preview_file_image").clone().appendTo(".add_preview").addClass('current_preview active_one').removeClass('preview_file_image');
+               let fileUrl = $(this)[0].src = URL.createObjectURL(this.files[0]);
+               $(".current_preview .preview_image").attr("src", fileUrl);
+            }
+            else if(jQuery.inArray(jQuery.trim(filename.split('.').pop().toLowerCase()), fileExtensionDocs) != -1) {
+                $('div').removeClass('current_preview');
+                $(".preview_file_docs").clone().appendTo(".add_preview").addClass('current_preview active_one').removeClass('preview_file_docs');
+               let fileUrl = $(this)[0].src = URL.createObjectURL(this.files[0]);
+               $(".current_preview .docs_priview_e").attr("src", fileUrl);
+            }
+            else{
+              alert('FILE TYPE NOT SUPPORTED');
+            }
 
+        })
     }
     return {
         //main function to initiate the module
